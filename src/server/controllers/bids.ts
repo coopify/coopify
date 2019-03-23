b7066fa0dccdf2970a47b9512b6b59dd21a62cdd
Juan Rodríguez <rodriguez.juan1795@gmail.com>
2019-03-23 13:12:53 -0300

    Working on creating a new bid

--- src/server/controllers/bids.ts
+++ src/server/controllers/bids.ts
@@ -21,9 +21,26 @@ export async function loadAsync(request: Request, response: Response, next: Next
 
 export async function createBidAsync(request: Request, response: Response) {
     try {
-        const bid: Bid = response.locals.bid
+        const loggedId = response.locals.loggedUser.userId
+        const { images, paymentMethod, startDate, finishDate, status } = request.body
+        if (!images || !paymentMethod || !startDate || !finishDate || !status) { 
+            throw new ErrorPayload(400, 'Missing required data')
+        }
 
-        const bodyResponse = {bid: Bid.toDTO(bid) }
+        const bidToCreate = await BidInterface.createAsync({
+            userId : loggedId,
+            description : request.body.description,
+            images : request.body.images,
+            category : request.body.category,
+            paymentMethod : request.body.paymentMethod,
+            startDate : request.body.startDate,
+            finishDate : request.body.finishDate,
+            status : request.body.status
+        })
+        
+        if (!bidToCreate) { throw new ErrorPayload(500, 'Failed to create a new bid') }
+
+        const bodyResponse = {bid: Bid.toDTO(bidToCreate) }
         response.status(200).json(bodyResponse)
     } catch (error) {
         handleError(error, response)
