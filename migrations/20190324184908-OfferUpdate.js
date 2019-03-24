d02aa34d558da2e8a0abdc2a11fe8fefb2f25292
Camilo <camilo.nicolas.martinez@gmail.com>
2019-03-24 15:49:32 -0300

    Added createdAt updatedAt columns

---
+++ migrations/20190324184908-OfferUpdate.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('OfferPrice', 'createdAt', Sequelize.DATE).then(() => {
+      queryInterface.addColumn('OfferPrice', 'updatedAt', Sequelize.DATE)
+    })
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.removeColumn('OfferPrice', 'createdAt').then(() => { queryInterface.removeColumn('OfferPrice', 'updatedAt') })
+  }
+};
