1ea0556e8085eebae420b83fb401a42e0891561d
Camilo <camilo.nicolas.martinez@gmail.com>
2019-04-23 21:37:41 -0300

    Added a new endpoint to get a single conversation

--- src/server/routes/conversation.ts
+++ src/server/routes/conversation.ts
@@ -4,9 +4,11 @@ import { conversationController, usersController } from '../controllers'
 const offerRoutes = Router()
 
 offerRoutes.get('/', conversationController.getListAsync)
+offerRoutes.get('/:conversationId', conversationController.getOneAsync)
 
 offerRoutes.post('/', usersController.authenticate, conversationController.createAsync)
 
 offerRoutes.param('offerId', conversationController.loadAsync)
+offerRoutes.param('conversarionId', conversationController.loadAsync)
 
 export default offerRoutes
