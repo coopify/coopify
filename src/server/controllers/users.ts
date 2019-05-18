94143a60bf4d5e7fc7d2a1a00b707565af570774
Camilo <camilo.nicolas.martinez@gmail.com>
2019-05-18 11:35:17 -0300

    Added an endpoint to sync a FB account to a user

--- src/server/controllers/users.ts
+++ src/server/controllers/users.ts
@@ -58,6 +58,26 @@ export async function exchangeFacebookCodeAsync(request: Request, response: Resp
     }
 }
 
+export async function syncFBAccountAsync(request: Request, response: Response) {
+    try {
+        const user: User = response.locals.loggedUser
+        const code = request.body.code
+        if (!code) { throw new ErrorPayload(400, 'Should provide a code to exchange') }
+        const tokens = await facebook.exchangeCodeAsync(code)
+        const userDataRaw = await facebook.getUserDataAsync(tokens.access_token)
+        const userData = facebook.transform(userDataRaw)
+        user.FBId = userData.FBId
+        user.FBAccessToken = tokens.access_token
+        user.FBAccessToken = tokens.refresh_token
+        const updatedUser = await UserInterface.updateAsync(user, user)
+        response.locals.user = updatedUser
+        response.status(200).json({ user: User.toDTO(user) })
+        response.send()
+    } catch (error) {
+        handleError(error, response)
+    }
+}
+
 export async function signupAsync(request: Request, response: Response, next: NextFunction) {
     try {
         const { email, referalCode } = request.body
