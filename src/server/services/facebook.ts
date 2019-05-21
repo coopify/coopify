6f3c847c0d513218c66b253161b8c9728aa1f663
Camilo <camilo.nicolas.martinez@gmail.com>
2019-05-21 20:30:23 -0300

    Fixes to the count token use

--- src/server/services/facebook.ts
+++ src/server/services/facebook.ts
@@ -89,9 +89,8 @@ export class FacebookService {
     }
 
     public async getPostStatsAsync(token: string, uri: string): Promise<any> {
-        FB.setAccessToken(token)
         return new Promise((resolve, reject) => {
-            FB.api(`/v3.2/?id=${uri}&fields=engagement&access_token=${token}`, (res) => {
+            FB.api(`/v3.2/?id=${uri}&fields=engagement&access_token=${this.options.apikey}|${this.options.secret}`, (res) => {
                 if (!res || res.error) {
                     logger.error(!res ? 'error occurred' : res.error)
                     throw new ErrorPayload(500, 'Facebook error', res.error)
