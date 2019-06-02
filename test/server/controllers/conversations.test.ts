16561c4195e9f5aa171d9bcbe32df83ac6336735
Juan Rodríguez <rodriguez.juan1795@gmail.com>
2019-06-02 13:47:10 -0300

    Fixes due to comments

--- test/server/controllers/conversations.test.ts
+++ test/server/controllers/conversations.test.ts
@@ -36,6 +36,10 @@ describe('Conversation Tests', async () => {
                     .send(createConversationClone).expect(200)
                 expect(res.body.conversations.length).to.eq(0)
             })
+            it('Should not be allow to access to the conversation', async () => {
+                const res = await request.get('/api/conversations/').expect(401)
+                expect(res.body.message).to.eq('Unauthorized you need to provide a valid token')
+            })
         })
     })
 
@@ -60,15 +64,11 @@ describe('Conversation Tests', async () => {
             })
         })
         context('No conversation previously created', async () => {
-            let createConversationClone, user, user2
+            let user
             beforeEach(async () => {
                 user = await factory.create('user', createUser)
-                user2 = await factory.create('user', createUser2)
-                createConversationClone = _.cloneDeep(createConversation)
-                createConversationClone.fromId = user.id
-                createConversationClone.toId = user2.id
             })
-            it.only('Should not get the conversation', async () => {
+            it('Should not get the conversation', async () => {
                 const conversationId: string = uuid()
                 const token = (await logInUser(user)).accessToken
                 const res = await request.get(`/api/conversations/${conversationId}`).expect(404)
@@ -85,15 +85,32 @@ describe('Conversation Tests', async () => {
                 user2 = await factory.create('user', createUser2)
                 createConversationClone = _.cloneDeep(createConversation)
                 createConversationClone.fromId = user.id
-                createConversationClone.toId = user2.id
             })
             it('Should create the new conversation', async () => {
+                createConversationClone.toId = user2.id
                 const token = (await logInUser(user)).accessToken
                 const res = await request.post('/api/conversations/').set('Authorization', `bearer ${token}`)
                     .send(createConversationClone).expect(200)
-                logger.info(`RES => ${JSON.stringify(res)}`)
                 expect(res.body.conversation.from).to.eq(createConversationClone.fromId)
                 expect(res.body.conversation.to).to.eq(createConversationClone.toId)
+            })
+            it('Should not create the new conversation', async () => {
+                createConversationClone.toId = user.id
+                const token = (await logInUser(user)).accessToken
+                const res = await request.post('/api/conversations/').set('Authorization', `bearer ${token}`)
+                    .send(createConversationClone).expect(400)
+                expect(res.body.message).to.eq('You cant start a conversation with your self')
+            })
+            it('Should create the new conversation despite there being one between the involved users', async () => {
+                createConversationClone.toId = user2.id
+                const token = (await logInUser(user)).accessToken
+                await request.post('/api/conversations/').set('Authorization', `bearer ${token}`)
+                    .send(createConversationClone)
+                const res = await request.post('/api/conversations/').set('Authorization', `bearer ${token}`)
+                    .send(createConversationClone).expect(200)
+                logger.info(`RES => ${JSON.stringify(res.body)}`)
+                expect(res.body.conversation.from.id).to.eq(user.id)
+                expect(res.body.conversation.to.id).to.eq(user2.id)
             })
         })
     })
