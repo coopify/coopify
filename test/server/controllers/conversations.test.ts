9571844041d77ba0586897650b280bae3b36c355
Juan Rodríguez <rodriguez.juan1795@gmail.com>
2019-06-02 11:01:56 -0300

    Conversation tests

--- test/server/controllers/conversations.test.ts
+++ test/server/controllers/conversations.test.ts
@@ -4,15 +4,95 @@ import * as supertest from 'supertest'
 import * as _ from 'lodash'
 import { logInUser } from './helpers'
 import { app } from '../../../src/server'
-import { factory, createUser, createOffer, createCategory, createQuestion } from '../factory'
+import { factory, createUser, createUser2, createConversation } from '../factory'
 import { logger } from '../../../src/server/services'
 
 const request = supertest(app)
 
 describe('Conversation Tests', async () => {
-    describe('#GET /api/converstions/', async () => { })
+    describe('#GET /api/conversations/', async () => {
+        context('Conversation already created', async () => {
+            let createConversationClone, user, user2
+            beforeEach(async () => {
+                user = await factory.create('user', createUser)
+                user2 = await factory.create('user', createUser2)
+            })
+            it('Should get the conversation list with one element', async () => {
+                const token = (await logInUser(user)).accessToken
+                createConversationClone = _.cloneDeep(createConversation)
+                createConversationClone.fromId = user.id
+                createConversationClone.toId = user2.id
+                await factory.create('conversation', createConversationClone)
+                const res = await request.get('/api/conversations/').set('Authorization', `bearer ${token}`)
+                    .send(createConversationClone).expect(200)
+                expect(res.body.conversations.length).to.eq(1)
+                expect(res.body.conversations[0].from.id).to.eq(createConversationClone.fromId)
+                expect(res.body.conversations[0].to.id).to.eq(createConversationClone.toId)
+            })
+            it('Should get an empty conversation list', async () => {
+                const token = (await logInUser(user)).accessToken
+                const res = await request.get('/api/conversations/').set('Authorization', `bearer ${token}`)
+                    .send(createConversationClone).expect(200)
+                expect(res.body.conversations.length).to.eq(0)
+            })
+        })
+    })
 
-    describe('#GET /api/conversations/:conversationId', async () => { })
+    describe('#GET /api/conversations/:conversationId', async () => {
+        context('Conversation already created', async () => {
+            let createConversationClone, conversationId, user, user2
+            beforeEach(async () => {
+                user = await factory.create('user', createUser)
+                user2 = await factory.create('user', createUser2)
+                createConversationClone = _.cloneDeep(createConversation)
+                createConversationClone.fromId = user.id
+                createConversationClone.toId = user2.id
+                const conversation = await factory.create('conversation', createConversationClone)
+                conversationId = conversation.id
+            })
+            it('Should get the conversation', async () => {
+                const token = (await logInUser(user)).accessToken
+                const res = await request.get(`/api/conversations/${conversationId}`).expect(200)
+                expect(res.body.conversation.id).to.eq(conversationId)
+                expect(res.body.conversation.from.id).to.eq(createConversationClone.fromId)
+                expect(res.body.conversation.to.id).to.eq(createConversationClone.toId)
+            })
+        })
+        context('No conversation previously created', async () => {
+            let createConversationClone, conversationId, user, user2
+            beforeEach(async () => {
+                user = await factory.create('user', createUser)
+                user2 = await factory.create('user', createUser2)
+                createConversationClone = _.cloneDeep(createConversation)
+                createConversationClone.fromId = user.id
+                createConversationClone.toId = user2.id
+                conversationId = '07fff370-afff-4374-ba61-93b33d0b8857'
+            })
+            it.only('Should not get the conversation', async () => {
+                const token = (await logInUser(user)).accessToken
+                const res = await request.get(`/api/conversations/${conversationId}`).expect(404)
+            })
+        })
+    })
 
-    describe('#POST /api/conversations/', async () => { })
+    describe('#POST /api/conversations/', async () => {
+        context('No conversation created', async () => {
+            let user, user2, createConversationClone
+            beforeEach(async () => {
+                user = await factory.create('user', createUser)
+                user2 = await factory.create('user', createUser2)
+                createConversationClone = _.cloneDeep(createConversation)
+                createConversationClone.fromId = user.id
+                createConversationClone.toId = user2.id
+            })
+            it('Should create the new conversation', async () => {
+                const token = (await logInUser(user)).accessToken
+                const res = await request.post('/api/conversations/').set('Authorization', `bearer ${token}`)
+                    .send(createConversationClone).expect(200)
+                logger.info(`RES => ${JSON.stringify(res)}`)
+                expect(res.body.conversation.from).to.eq(createConversationClone.fromId)
+                expect(res.body.conversation.to).to.eq(createConversationClone.toId)
+            })
+        })
+    })
 })
